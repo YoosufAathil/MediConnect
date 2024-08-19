4835febfd26fc4606c356e2ae6cd34fe5ee9e34d
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-08-19 20:55:41 +0530

    chore: Add PatientForm component and update page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import { Button } from "@/components/ui/button";
+import PatientForm from "@/components/ui/forms/PatientForm";
 import Image from "next/image";
+import Link from "next/link";
+import onbording from "../public/assets/images/onboarding-img.png";
 
 export default function Home() {
   return (
@@ -13,8 +16,24 @@ export default function Home() {
             alt="patient"
             className="mb-12 h-10 w-fit"
           />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              ©MediConnect
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+      <Image
+        src={onbording}
+        alt="patient"
+        height={1000}
+        width={1000}
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
