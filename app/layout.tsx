6fa319416693f0da5f96c13a27dcc4069a6a4b0a
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-08-19 20:06:59 +0530

    Add Plus_Jakarta_Sans font and ThemeProvider component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,20 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+
+const fontSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "MediConnect",
-  description: "MediConnect is a healthcare system designed for efficient patient management, appointment scheduling, and medical record tracking. Built with modern web technologies, it offers a responsive and secure platform for healthcare providers and patients.",
+  description:
+    "MediConnect is a healthcare system designed for efficient patient management, appointment scheduling, and medical record tracking. Built with modern web technologies, it offers a responsive and secure platform for healthcare providers and patients.",
 };
 
 export default function RootLayout({
@@ -16,7 +24,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
