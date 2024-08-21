4c93edf2995dd0aa94f9987891c0c9df24313834
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-08-21 17:49:18 +0530

    feat: Refactor createUser function in patient.actions.ts

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,7 +1,13 @@
-export const createUser = async (user : CreateUserParams){
-    try {
-        
-    } catch (error) {
-        
+import { Query } from "node-appwrite";
+import { users } from "../appwrite.config";
+
+export const createUser = async (user: CreateUserParams) => {
+  try {
+  } catch (error) {
+    if (error && error?.code === 409) {
+      const existingUser = await users.list([
+        Query.equal("email", [user.email]),
+      ]);
     }
-}
+  }
+};
